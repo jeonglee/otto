cd821435571f663748fb34f4e377f2be8b3d2f53
Nick Samson <nes77@cornell.edu>
2016-11-06 10:48:26 -0500

    Update errable.mli

--- errable.mli
+++ errable.mli
@@ -5,6 +5,12 @@ type 'a errable =
   | Ok of 'a
   | Err of exn
 
+(* a >>= f takes an errable and passes it into function f, producing another errable *)
 val (>>=) : 'a errable -> ('a -> 'b errable) -> 'b errable
+
+(* a >>> f takes an errable and passes it into the non-errable function f,
+ * producing an errable if f raises and exception. *)
 val (>>>) : 'a errable -> ('a -> 'b) -> 'b errable
+
+(* ?!a extracts the value from an errable, or raises the exception inside it. *)
 val (?!) : 'a errable -> 'a
