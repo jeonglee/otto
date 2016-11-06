fe9d648dd0f8299d35bf85b74f0eba133f14860f
Nick Samson <nes77@cornell.edu>
2016-11-06 10:55:36 -0500

    Update fileCrawler.mli

--- fileCrawler.mli
+++ fileCrawler.mli
@@ -1,7 +1,12 @@
 open Errable
 
+(* a file is a tuple of its name and its base64 encoded contents. *)
 type file = string * string
 
+(* takes the name of a directory and creates a file list errable containing
+ * the file names and their contents. Produces an Err describing the failure if it
+ * fails *)
 val files_from_dir : string -> file list errable
 
-val write_file : file -> unit errable
+(* writes a file to disk, using the file name as the location to write to *)
+val write_file : ?dir:string -> file -> unit errable
