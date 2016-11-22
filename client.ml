244dce5e1c7ff3964e75789b67712a87d4d24bfe
meh339 <meh339@cornell.edu>
2016-11-21 19:47:53 -0500

    Added template for client.ml

---
+++ client.ml
@@ -0,0 +1,36 @@
+open Errable
+
+type config = {
+  remote_port : int; (* Base port the C&C server is running on *)
+  remote_ip : Message.ip; (* IP of the C&C server *)
+  test_dir : string; (* Directory in which to download files and run tests *)
+}
+
+(* Pulls tests from the C&C server, runs them, and responds to the server with
+ * a summary of the test results. *)
+module type Client = sig
+  type t
+
+  (* make initializes a new client *)
+  val make : config -> t errable
+
+  (* main runs the client until the testing server goes down or
+   * broadcasts the fact that there are no more tests to run. *)
+  val main : t -> unit errable
+
+  (* Frees any resources held by a client. *)
+  val close : t -> t
+
+end
+
+module ClientImpl : Client = struct
+  type t = ()
+
+  let make config = failwith "unimplemented"
+
+  let main c = failwith "unimplemented"
+
+  let close c = failwith "unimplemented"
+
+end
+
