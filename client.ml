dabb73da7090568aa709626e8ef9636e23cd23bc
meh339 <meh339@cornell.edu>
2016-11-28 15:14:50 -0500

    Updated client type and added heartbeat thread

--- client.ml
+++ client.ml
@@ -1,5 +1,8 @@
 open Errable
+open Errable.M
 open Comm
+open Util
+open Async
 
 type config = {
   remote_port : int; (* Base port the C&C server is running on *)
@@ -35,35 +38,78 @@ module ClientImpl : Client = struct
     hb_resp     : 'a ReqCtxt.t;    (* For responding to the heartbeat *)
     pull        : 'a PullCtxt.t;   (* For pulling tests *)
     file_req    : 'a ReqCtxt.t;    (* For getting files to grade *)
-    return      : 'a ReqCtxt.t     (* For returning graded results *)
+    return      : 'a ReqCtxt.t;    (* For returning graded results *)
+    (* Add a thread for dealing with heartbeat *)
+    (* Add a thread for dealing with timeout *)
+    (* Add a mutex for accessing the DONE field *)
+    (* Add the DONE field *)
+    finished    : bool ref;
+    fin_lock    : Mutex.t;
+    hb_thread   : Thread.t Once.t;
   } constraint 'a = [> `Req | `Sub | `Pull ]
 
-  let make conf =
-    let o = {
-      conf        = conf;
-      remote_port = conf.remote_port;
-      remote_ip   = conf.remote_ip;
-      test_dir    = conf.test_dir;
-      sub         = SubCtxt.make {port = conf.remote_port;
-                                  remote_ip = conf.remote_ip};
-      pull        = PullCtxt.make {port = conf.remote_port + 1;
-                                  remote_ip = conf.remote_ip};
-      file_req    = ReqCtxt.make {port = conf.remote_port + 2;
-                                  remote_ip = conf.remote_ip};
-      return      = ReqCtxt.make {port = conf.remote_port + 3;
-                                  remote_ip = conf.remote_ip};
-      hb_resp     = ReqCtxt.make {port = conf.remote_port + 4;
-                                  remote_ip = conf.remote_ip};
-    } in
-    Ok o (*!!!!!! Do some checking here - just did Ok to make it compile for now !!!!!!!! *)
+  let get_ip =
+    try (Ok (Unix.open_process_in "curl \"https://api.ipify.org\" 2>/dev/null"
+      |> input_line))
+    with
+    | End_of_file -> Err End_of_file
 
+  (* Helper function for receiving and responding to heartbeats *)
+  (* This will also be responsible for checking when grading is done *)
+  (* When it is, it should set the value at [finished] to true *)
+  let hb_handler c (u : unit) =
+    let open Message in
+    let check_if_done m =
+      match m with
+      | Heartbeat(time,d) when (d = true) ->
+          Mutex.lock c.fin_lock;
+          c.finished:=true;
+          Mutex.unlock c.fin_lock;
+      | Heartbeat(time, d) -> ()
+      | _ -> raise Comm.Invalid_ctxt
+    in
+    SubCtxt.connect check_if_done c.sub;
+    let unpack_ip packed = match packed with
+    | Ok ip -> ip
+    | Err e -> raise e in
+    let req = HeartbeatResp (unpack_ip get_ip) in
+    match (ReqCtxt.send req c.hb_resp) with
+    | Ok _ -> ()
+    | Err e -> raise e
+
+  let make conf =
+    try
+      let o = {
+        conf        = conf;
+        remote_port = conf.remote_port;
+        remote_ip   = conf.remote_ip;
+        test_dir    = conf.test_dir;
+        sub         = SubCtxt.make {port = conf.remote_port;
+                                    remote_ip = conf.remote_ip};
+        pull        = PullCtxt.make {port = conf.remote_port + 1;
+                                    remote_ip = conf.remote_ip};
+        file_req    = ReqCtxt.make {port = conf.remote_port + 2;
+                                    remote_ip = conf.remote_ip};
+        return      = ReqCtxt.make {port = conf.remote_port + 3;
+                                    remote_ip = conf.remote_ip};
+        hb_resp     = ReqCtxt.make {port = conf.remote_port + 4;
+                                    remote_ip = conf.remote_ip};
+        finished    = ref false;
+        fin_lock    = Mutex.create ();
+        hb_thread   = Once.make ();
+      } in
+      Once.set (!-> (hb_handler o)) o.hb_thread;
+      Ok o (*!!!!!! Do some checking here - just did Ok to make it compile for now !!!!!!!! *)
+    with e -> Err e
 
   (* execute pulled commands and returns unit if successful, and raises failure
    * otherwise. A command execution is 'successful' if its exit code is 0 *)
   let execute commands =
+
     let exit_codes = List.map Sys.command commands in
     let sum = List.fold_left (+) 0 exit_codes in
     if sum = 0 then () else failwith "failed to execute pulled commands"
+
 
   (* Takes in FileCrawler.files and turns them into
    * actual files in the current working directory *)
@@ -103,43 +149,8 @@ module ClientImpl : Client = struct
     Unix.dup2 old Unix.stdout;
     results
 
-  let timeout t (u : unit) = failwith "unimplemented"
-  (* TODO: helper function to implement timing out *)
-  (* This should be run in its own thread during execute *)
-  (* The thread should then be killed at the end of execute *)
-
-
-  let get_ip =
-    try (Ok (Unix.open_process_in "curl \"https://api.ipify.org\" 2>/dev/null"
-      |> input_line))
-    with
-    | End_of_file -> Err End_of_file
-
-  (* TODO: helper function for receiving and responding to heartbeats *)
-  (* This will also be responsible for checking when grading is done *)
-  (* When it is, it should set the value at [finished] to true *)
-  let hb_handler c finished (u : unit) =
-    let open Message in
-    let check_if_done m =
-      match m with
-      | Heartbeat(time,d) when (d = true) -> finished:=true;
-      | _ -> raise Comm.Invalid_ctxt
-    in
-    SubCtxt.connect check_if_done c.sub;
-    let unpack_ip packed = match packed with
-    | Ok ip -> ip
-    | Err e -> raise e in
-    let req = HeartbeatResp (unpack_ip get_ip) in
-    match (ReqCtxt.send req c.hb_resp) with
-    | Ok _ -> ()
-    | Err e -> raise e
-
   let main c =
-    (* TODO: set up a thread running the heartbeat check function *)
-    let finished = ref false in
-    let hb = Async.run_in_background (hb_handler c finished) in
-
-    let rec main_loop c finished =
+    let rec main_loop (c : 'a t) =
       let open Message in
       let netid = ref "" in
       let timeout = ref (-1) in
@@ -149,7 +160,6 @@ module ClientImpl : Client = struct
         | _ -> raise Comm.Invalid_ctxt
       in
       let () = PullCtxt.connect do_on_pull c.pull in
-      (*---------- everything for pull up to here ----------*)
       let files = ref [] in
       let req_mes = FileReq (!netid) in
       match ReqCtxt.send req_mes c.file_req with
@@ -157,10 +167,11 @@ module ClientImpl : Client = struct
       | Ok (Files f) -> files := f;
                         let results = run_tests (!netid) (!files) (!commands) in
                         let res_mes = TestCompletion (!netid, results) in
-                        let ack = ReqCtxt.send res_mes c.return in
-                        if !finished then Ok () else main_loop c finished
+                        let _ = ReqCtxt.send res_mes c.return in
+                        if !(c.finished) then Ok ()
+                        else main_loop c
       | Ok _ -> failwith "unexpected response"
-    in main_loop c finished
+    in main_loop c
 
   let close c =
     let s = SubCtxt.close c.sub in
@@ -177,9 +188,15 @@ module ClientImpl : Client = struct
       pull        = p;
       file_req    = f;
       return      = r;
-      hb_resp     = h
+      hb_resp     = h;
+      finished    = c.finished;
+      fin_lock    = c.fin_lock;
+      hb_thread   = c.hb_thread;
     } in
+    ??? (Once.coerce o.hb_thread);
+    Mutex.lock o.fin_lock;
+    o.finished := true;
+    Mutex.unlock o.fin_lock;
     o
-
 
 end
